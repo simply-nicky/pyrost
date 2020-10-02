6dfc12f538670b3a76761d168b15431480236f63
Nikolay Ivanov <simply_nicky@nikolays-mbp.localdomain>
2020-10-02 18:00:57 +0200

    Robust Speckle Tracking implementation

--- st_sim/bin/st_utils.pyx
+++ st_sim/bin/st_utils.pyx
@@ -1,7 +1,7 @@
 cimport numpy as cnp
 import numpy as np
 from cython_gsl cimport *
-from libc.math cimport sqrt, cos, sin, exp, pi
+from libc.math cimport sqrt, cos, sin, exp, pi, floor
 from cython.parallel import prange
 cimport openmp
 
@@ -11,6 +11,8 @@ ctypedef cnp.float32_t float32_t
 ctypedef cnp.int64_t int_t
 ctypedef cnp.uint64_t uint_t
 ctypedef cnp.uint8_t uint8_t
+ctypedef cnp.npy_bool bool_t
+DEF FLOAT_MAX = 1.7976931348623157e+308
 
 cdef float32_t wirthselect_float(float32_t[:] array, int k) nogil:
     cdef:
@@ -30,7 +32,7 @@ cdef float32_t wirthselect_float(float32_t[:] array, int k) nogil:
         if k < i: m = j 
     return array[k]
 
-def make_whitefield_st(float32_t[:, :, ::1] data, uint8_t[:, ::1] mask):
+def make_whitefield_st(float32_t[:, :, ::1] data, bool_t[:, ::1] mask):
     """
     Return whitefield based on median filtering of the stack of frames
 
@@ -95,28 +97,158 @@ cdef void frame_reference(float_t[:, ::1] I0, float_t[:, ::1] w0, float_t[:, ::1
                     I0[jj, kk] += I[j, k] * W[j, k] * rbf(ss - jj, ls) * rbf(fs - kk, ls)
                     w0[jj, kk] += W[j, k]**2 * rbf(ss - jj, ls) * rbf(fs - kk, ls)
 
-def make_reference(float_t[:, :, ::1] I_n, float_t[:, ::1] W, float_t[:, ::1] dij, float_t[:, :, ::1] u, float_t ls):
+def make_reference(float_t[:, :, ::1] I_n, float_t[:, ::1] W, float_t[:, :, ::1] u,
+                   float_t[::1] di, float_t[::1] dj, float_t ls=2.5):
     """
     Return a reference image 
 
     I_n - measured data
     W - whitefield
-    dij - sample translations
     u - pixel map
+    di, dj - sample translations along slow and fast axis in pixels
     ls - length scale in pixels
     """
     cdef:
         int_t a = I_n.shape[0], b = I_n.shape[1], c = I_n.shape[2], i, j
-        float_t n0 = -min_float(&u[0, 0, 0], b * c) + max_float(&dij[0, 0], a)
-        float_t m0 = -min_float(&u[1, 0, 0], b * c) + max_float(&dij[1, 0], a)
-        int_t aa = <int_t>(max_float(&u[0, 0, 0], b * c) - min_float(&dij[0, 0], a) + n0) + 1
-        int_t bb = <int_t>(max_float(&u[1, 0, 0], b * c) - min_float(&dij[1, 0], a) + m0) + 1
+        float_t n0 = -min_float(&u[0, 0, 0], b * c) + max_float(&di[0], a)
+        float_t m0 = -min_float(&u[1, 0, 0], b * c) + max_float(&dj[0], a)
+        int_t aa = <int_t>(max_float(&u[0, 0, 0], b * c) - min_float(&di[0], a) + n0) + 1
+        int_t bb = <int_t>(max_float(&u[1, 0, 0], b * c) - min_float(&dj[0], a) + m0) + 1
         float_t[:, ::1] I0 = np.zeros((aa, bb), dtype=np.float64)
         float_t[:, ::1] w0 = np.zeros((aa, bb), dtype=np.float64)
     for i in range(a):
-        frame_reference(I0, w0, I_n[i], W, u, dij[0, i] - n0, dij[1, i] - m0, ls)
+        di[i] = di[i] - n0; dj[i] = dj[i] - m0
+        frame_reference(I0, w0, I_n[i], W, u, di[i], dj[i], ls)
     for i in range(aa):
         for j in range(bb):
             if w0[i, j]:
                 I0[i, j] /= w0[i, j]
-    return np.asarray(I0), n0, m0
+    return np.asarray(I0), np.asarray(di), np.asarray(dj)
+
+cdef float_t mse_bi(float_t[:] I, float_t[:, ::1] I0, float_t W, float_t[::1] di,
+                    float_t[::1] dj, float_t ux, float_t uy) nogil:
+    cdef:
+        int_t a = I.shape[0], aa = I0.shape[0], bb = I0.shape[1]
+        int_t i, ss0, ss1, fs0, fs1
+        float_t mse = 0, var = 0, ss, fs, dss, dfs, I0_bi
+    for i in range(a):
+        ss = ux - di[i]
+        fs = uy - dj[i]
+        if ss >= 0 and ss <= aa - 1 and fs >= 0 and fs <= bb - 1:
+            dss = ss - floor(ss)
+            dfs = fs - floor(fs)
+            if dss:
+                ss0 = <int_t>(floor(ss)); ss1 = ss0 + 1
+            else:
+                ss0 = <int_t>(ss); ss1 = ss0
+            if dfs:
+                fs0 = <int_t>(floor(fs)); fs1 = fs0 + 1
+            else:
+                fs0 = <int_t>(fs); fs1 = fs0
+            I0_bi = (1 - dss) * (1 - dfs) * I0[ss0, fs0] + \
+                    (1 - dss) * dfs * I0[ss0, fs1] + \
+                    dss * (1 - dfs) * I0[ss1, fs0] + \
+                    dss * dfs * I0[ss1, fs1]
+            mse += (I[i] - W * I0_bi)**2
+            var += (I[i] - W)**2
+    return mse / var
+
+cdef float_t mse_nobi(float_t[:] I, float_t[:, ::1] I0, float_t W, float_t[::1] di,
+                      float_t[::1] dj, float_t ux, float_t uy) nogil:
+    cdef:
+        int_t a = I.shape[0], aa = I0.shape[0], bb = I0.shape[1]
+        int_t i, ss0, fs0
+        float_t mse = 0, var = 0, ss, fs
+    for i in range(a):
+        ss = ux - di[i]
+        fs = uy - dj[i]
+        if ss >= 0 and ss <= aa - 1 and fs >= 0 and fs <= bb - 1:
+            ss0 = <int_t>(floor(ss))
+            fs0 = <int_t>(floor(fs))
+            mse += (I[i] - W * I0[ss0, fs0])**2
+            var += (I[i] - W)**2
+    return mse / var
+
+cdef void search_2d(float_t[:] I, float_t[:, ::1] I0, float_t[::1] di, float_t[::1] dj,
+                    float_t[:] u, float_t W, int_t i0, int_t i1, int_t j0, int_t j1,
+                    float_t dss, float_t dfs) nogil:
+    cdef:
+        int_t j = j0, i_min = 0, j_min = 0, i, jj
+        float_t mse_min = FLOAT_MAX, mse = 0, df_ss, df_fs
+    for i in range(i0, i1):
+        if i - i0:
+            df_ss = (mse_bi(I, I0, W, di, dj, u[0] + i - 0.5 + dss, u[1] + j) - \
+                     mse_bi(I, I0, W, di, dj, u[0] + i - 0.5 - dss, u[1] + j)) / 2 / dss
+            mse += df_ss
+            if mse < mse_min:
+                mse_min = mse; i_min = i; j_min = j
+        if (i - i0) % 2:
+            for jj in range(j0 + 1, j1):
+                j = j1 + j0 - jj - 1
+                df_fs = (mse_bi(I, I0, W, di, dj, u[0] + i, u[1] + j - 0.5 + dfs) - \
+                         mse_bi(I, I0, W, di, dj, u[0] + i, u[1] + j - 0.5 - dfs)) / 2 / dfs
+                mse += df_fs
+                if mse < mse_min:
+                    mse_min = mse; i_min = i; j_min = j
+        else:
+            for j in range(j0 + 1, j1):
+                df_fs = (mse_bi(I, I0, W, di, dj, u[0] + i, u[1] + j - 0.5 + dfs) - \
+                         mse_bi(I, I0, W, di, dj, u[0] + i, u[1] + j - 0.5 - dfs)) / 2 / dfs
+                mse += df_fs
+                if mse < mse_min:
+                    mse_min = mse; i_min = i; j_min = j
+    u[0] += i_min; u[1] += j_min
+
+def update_pixel_map_search(float_t[:, :, ::1] I_n, float_t[:, ::1] W, float_t[:, ::1] I0,
+                            float_t[:, :, ::1] u0, float_t[::1] di, float_t[::1] dj,
+                            float_t dss, float_t dfs, uint_t wss=1, uint_t wfs=100):
+    """
+    Update the pixel map
+
+    I_n - measured data
+    W - whitefield
+    I0 - reference image
+    u0 - pixel map
+    di, dj - sample translations along slow and fast axis in pixels
+    dss, dfs - average sample translations along slow and fast axis in pixels
+    wss, wfs - search window size along slow and fast axis
+    """
+    cdef:
+        int_t a = I_n.shape[0], b = I_n.shape[1], c = I_n.shape[2], j, k
+        int_t aa = I0.shape[0], bb = I0.shape[1]
+        float_t di0 = min_float(&di[0], a), di1 = max_float(&di[0], a)
+        float_t dj0 = min_float(&dj[0], a), dj1 = max_float(&dj[0], a)
+        float_t i0, i1, j0, j1
+        float_t[:, :, ::1] u = np.empty((2, b, c), dtype=np.float64)
+    u[...] = u0
+    for j in range(b):
+        for k in range(c):
+            # Define window search bounds
+            i0 = -<float_t>(wss) if wss < u[0, j, k] - di0 else di0 - u[0, j, k]
+            i1 = <float_t>(wss) if wss < di1 - u[0, j, k] + aa else di1 - u[0, j, k] + aa
+            j0 = -<float_t>(wfs) if wfs < u[1, j, k] - dj0 else dj0 - u[1, j, k]
+            j1 = <float_t>(wfs) if wfs < dj1 - u[1, j, k] + bb else dj1 - u[1, j, k] + bb
+            
+            # Execute pixel map search
+            search_2d(I_n[:, j, k], I0, di, dj, u[:, j, k], W[j, k],
+                      <int_t>(i0), <int_t>(i1), <int_t>(j0), <int_t>(j1), dss, dfs)
+    return np.asarray(u)
+
+def total_mse(float_t[:, :, ::1] I_n, float_t[:, ::1] W, float_t[:, ::1] I0,
+              float_t[:, :, ::1] u, float_t[::1] di, float_t[::1] dj):
+    """
+    Return total mean squared error
+
+    I_n - measured data
+    W - whitefield
+    I0 - reference image
+    u - pixel map
+    di, dj - sample translations along slow and fast axis in pixels
+    """
+    cdef:
+        int_t b = I_n.shape[1], c = I_n.shape[2], j, k
+        float_t err = 0
+    for j in range(b):
+        for k in range(c):
+            err += mse_bi(I_n[:, j, k], I0, W[j, k], di, dj, u[0, j, k], u[1, j, k])
+    return err / b / c
--- st_sim/st_loader.py
+++ st_sim/st_loader.py
@@ -1,10 +1,14 @@
 import os
 import h5py
 import numpy as np
+from scipy.ndimage import gaussian_filter
 from .st_wrapper import cxi_protocol, INIParser, ROOT_PATH
-from .bin import make_whitefield_st, make_reference
+from .bin import make_whitefield_st, make_reference, update_pixel_map_search, total_mse
 
 class STLoader(INIParser):
+    """
+    Speckle Tracking scan loader class
+    """
     attr_dict = {'paths': ('ALL',)}
     fmt_dict = {'paths': 'list'}
 
@@ -30,9 +34,9 @@ class STLoader(INIParser):
 
     def load(self, path, defocus=None, roi=None):
         """
-        Load a cxi file
+        Load a cxi file and return a STData object
         """
-        data_dict = {'protocol': self.protocol}
+        data_dict = {}
         if roi:
             data_dict['roi'] = np.asarray(roi, dtype=self.protocol.dtypes['roi'])
         if defocus:
@@ -42,7 +46,7 @@ class STLoader(INIParser):
                 cxi_path = self.find_path(attr, cxi_file)
                 if cxi_path:
                     data_dict[attr] = cxi_file[cxi_path][...].astype(self.protocol.dtypes[attr])
-        return data_dict
+        return STData(self.protocol, **data_dict)
 
 def loader():
     """
@@ -55,21 +59,46 @@ class dict_to_object:
         self.finstance = finstance
 
     def __get__(self, instance, cls):
-        return return_obj_method(self.finstance.__get__(instance, cls), cls)
+        return return_obj_method(self.finstance.__get__(instance, cls), cls, instance)
 
 class return_obj_method:
-    def __init__(self, dict_func, cls):
-        self.dict_func, self.cls = dict_func, cls
+    def __init__(self, dict_func, cls, instance):
+        self.dict_func, self.cls, self.instance = dict_func, cls, instance
 
     def __call__(self, *args, **kwargs):
-        return self.cls(**self.dict_func(*args, **kwargs))
+        dct = self.instance.__dict__.copy()
+        dct.update(self.dict_func(*args, **kwargs))
+        return self.cls(**dct)
+
+    def inplace_update(self, *args, **kwargs):
+        self.instance.__dict__.update(self.dict_func(*args, **kwargs))
 
 class STData:
+    """
+    Speckle Tracking scan data class
+
+    Necessary attributes:
+    basis_vectors - detector basis vectors
+    data - measured data
+    defocus - defocus distance
+    distance - sample-to-detector distance
+    translations - sample translations
+    wavelength - incoming beam wavelength
+    x_pixel_size - slow axis pixel size
+    y_pixel_size - fast axis pixel size
+
+    Optional attributes:
+    good_frames - good frames array
+    mask - bad pixels mask
+    pixel_translations - sample translations in the detector plane in pixels
+    roi - region of interest
+    whitefield - whitefield
+    """
     attr_dict = {'basis_vectors', 'data', 'defocus', 'distance',
                  'translations', 'wavelength', 'x_pixel_size',
                  'y_pixel_size'}
-    init_dict = {'good_frames', 'm0', 'mask', 'n0', 'pixel_translations',
-                 'reference_image', 'roi', 'whitefield'}
+    init_dict = {'good_frames', 'mask', 'pixel_translations',
+                 'roi', 'whitefield'}
 
     def __init__(self, protocol=cxi_protocol(), **kwargs):
         self.protocol = protocol
@@ -88,7 +117,7 @@ class STData:
         # Set pixel translations
         if self.pixel_translations is None:
             self.pixel_translations = (self.translations[:, None] * self.basis_vectors).sum(axis=-1)
-            self.pixel_translations /= np.sqrt((self.basis_vectors**2).sum(axis=-1)) * self.defocus
+            self.pixel_translations /= (self.basis_vectors**2).sum(axis=-1) * self.defocus / self.distance
             self.pixel_translations -= self.pixel_translations.mean(axis=0)
 
         # Set good frames array and mask
@@ -97,45 +126,173 @@ class STData:
         if self.mask is None or self.whitefield is None:
             self.mask = np.ones(self.data.shape[1:],
                                 dtype=self.protocol.dtypes['mask'])
-            self.whitefield = make_whitefield_st(data=self.data,
-                                                 mask=self.mask.astype(np.uint8))
-
-        # Apply ROI
-        if self.roi is None:
-            self.roi = np.array([0, self.data.shape[1], 0, self.data.shape[2]],
-                                dtype=self.protocol.dtypes['roi'])
-        self.data_st = self.data[:, self.roi[0]:self.roi[1], self.roi[2]:self.roi[3]].sum(axis=1)[:, None]
-        self.mask_st = np.any(self.mask[self.roi[0]:self.roi[1]], axis=0)[None, self.roi[2]:self.roi[3]]
-        self.w_st = make_whitefield_st(data=self.data_st, mask=self.mask_st.astype(np.uint8))
-        self.pixel_map = np.indices(self.w_st.shape)
-        self.n0 = -np.min(self.pixel_map[0]) + np.max(self.pixel_translations[:, 0])
-        self.m0 = -np.min(self.pixel_map[1]) + np.max(self.pixel_translations[:, 1])
+            self.whitefield = make_whitefield_st(data=self.data[self.good_frames], mask=self.mask)
 
     @dict_to_object
-    def change_roi(self, roi):
+    def crop_data(self, roi):
         """
         Return new STData object with new ROI
         """
-        dct = self.__dict__.copy()
-        dct['roi'] = roi
-        return dct
+        return {'roi': np.asarray(roi, dtype=np.int)}
+
+    @dict_to_object
+    def mask_frames(self, good_frames):
+        """
+        Return new STData object with new good frames mask
+        """
+        return {'good_frames': np.asarray(good_frames, dtype=np.int),
+                'whitefield': make_whitefield_st(data=self.data[good_frames], mask=self.mask)}
 
     @dict_to_object
     def change_defocus(self, defocus):
         """
-        Return new STData object with new ROI
+        Return new STData with new defocus distance
         """
-        dct = self.__dict__.copy()
-        dct['defocus'] = defocus
-        dct['pixel_translations'] *= self.defocus / defocus
+        return {'defocus': defocus, 'pixel_translations': \
+                self.pixel_translations * self.defocus / defocus}
+
+    def st_process(self):
+        """
+        Return a SpeckleTracking1D object
+        """
+        return SpeckleTracking1D.import_dict(**self.__dict__)
+
+class SpeckleTracking1D:
+    """
+    One-dimensional Robust Speckle Tracking Algorithm
+
+    roi - Region Of Interest
+    good_frames - array with good frames indexes
+    data - measured frames
+    whitefield - whitefield
+    dss_pix, dfs_pix - sample translations along the slow adn fast axes in pixels
+    dss_avg, dfs_avg - average sample translation along the slow and fast axes in pixels
+    pix_map - pixel map
+    ref_img - reference image
+    """
+    attr_dict = {'roi', 'good_frames', 'data', 'whitefield', 'dss_pix', 'dfs_pix'}
+    init_dict = {'pix_map', 'ref_img', 'dss_avg', 'dfs_avg'}
+
+    def __init__(self, **kwargs):
+        for attr in self.attr_dict | self.init_dict:
+            if attr in kwargs:
+                self.__dict__[attr] = kwargs[attr]
+            elif attr in self.init_dict:
+                self.__dict__[attr] = None
+            else:
+                raise ValueError('{0:s} has not been provided'.format(attr))
+        
+        if self.dss_avg is None or self.dfs_avg is None:
+            self.dfs_avg = np.mean(self.dfs_pix[1:] - self.dfs_pix[:-1])
+            self.dss_avg = np.mean(self.dss_pix[1:] - self.dss_pix[:-1])
+        if self.pix_map is None:
+            self.pix_map = np.indices(self.whitefield.shape, dtype=np.float64)
+        if self.ref_img is None:
+            self.ref_img, self.dss_pix, self.dfs_pix = \
+            make_reference(I_n=self.data, W=self.whitefield,
+                           u=self.pix_map, di=self.dss_pix,
+                           dj=self.dfs_pix)
+
+    @classmethod
+    def import_dict(cls, **kwargs):
+        """
+        Return a SpeckleTracking object from a data dictionary with the given items:
+
+        roi - Region Of Interest
+        good_frames - array with good frames indexes
+        mask - bad pixels mask
+        data - measured frames
+        whitefield - whitefield
+        pixel_translations - sample translations in pixels
+        """
+        # fetch the data
+        roi, good_frames = kwargs['roi'], kwargs['good_frames']
+        dct = {'roi': roi, 'good_frames': good_frames}
+        mask = kwargs['mask'][roi[0]:roi[1], roi[2]:roi[3]]
+        data = np.ascontiguousarray(kwargs['data'][good_frames, roi[0]:roi[1], roi[2]:roi[3]])
+        dct['data'] = (mask * data).sum(axis=1)[:, None].astype(np.float64)
+        dct['whitefield'] = kwargs['whitefield'][roi[0]:roi[1], roi[2]:roi[3]].astype(np.float64)
+        dct['dss_pix'] = kwargs['pixel_translations'][good_frames, 0]
+        dct['dfs_pix'] = kwargs['pixel_translations'][good_frames, 1]
+        return cls(**dct)
+
+    @dict_to_object
+    def crop_data(self, roi):
+        """
+        Return new object with the data cropped according to the given ROI
+        """
+        dct = {'roi': np.array([roi[0] + self.roi[0], roi[1] + self.roi[0],
+                                roi[2] + self.roi[2], roi[3] + self.roi[2]])}
+        dct['data'] = np.ascontiguousarray(self.data[:, roi[0]:roi[1], roi[2]:roi[3]])
+        dct['pix_map'] = np.ascontiguousarray(self.pix_map[:, roi[0]:roi[1], roi[2]:roi[3]])
+        dct['pix_map'][0] -= roi[0]; dct['pix_map'][1] -= roi[2]
+        dct['whitefield'] = self.whitefield[roi[0]:roi[1], roi[2]:roi[3]]
+        dct['ref_img'] = self.ref_img[roi[0]:roi[1], roi[2]:roi[3]]
         return dct
 
-    # @dict_to_object
-    # def make_reference(self, l_scale=2.5):
-    #     """
-    #     Generate the reference image
+    @dict_to_object
+    def mask_frames(self, good_frames):
+        """
+        Return new object with the frames masked according to the given good frames array
+        """
+        return {'good_frames': self.good_frames[good_frames],
+                'data': self.data[good_frames],
+                'dss_pix': self.dss_pix[good_frames],
+                'dfs_pix': self.dfs_pix[good_frames]}
 
-    #     l_scale - kernel length scale in pixels
-    #     """
-    #     dct = self.__dict__.copy()
-    #     dct['reference_image'] = make_reference()
+    @dict_to_object
+    def update_reference(self, l_scale=2.5):
+        """
+        Return new object with the updated reference image
+
+        l_scale - length scale in pixels
+        """
+        dct = {}
+        dct['ref_img'], dct['dss_pix'], dct['dfs_pix'] = \
+        make_reference(I_n=self.data, W=self.whitefield,
+                       u=self.pix_map, di=self.dss_pix,
+                       dj=self.dfs_pix, ls=l_scale)
+        return dct
+
+    @dict_to_object
+    def update_pixel_map(self, wfs, l_scale=2.5):
+        """
+        Return new object with the updated pixel map
+
+        wfs - search window size in pixels
+        l_scale - length scale in pixels
+        """
+        pix_map = update_pixel_map_search(I_n=self.data, W=self.whitefield, I0=self.ref_img,
+                                          u0=self.pix_map, di=self.dss_pix, dj=self.dfs_pix,
+                                          dss=self.dss_avg, dfs=self.dfs_avg, wss=1, wfs=wfs // 2)
+        return {'pix_map': gaussian_filter(pix_map, (0, 0, l_scale))}
+
+    def mse(self):
+        """
+        Return mean-squared-error (MSE)
+        """
+        return total_mse(I_n=self.data, W=self.whitefield, I0=self.ref_img,
+                         u=self.pix_map, di=self.dss_pix, dj=self.dfs_pix)
+
+    def iter_update(self, wfs, l_scale=2.5, n_iter=5, verbose=True):
+        """
+        Update the reference image and the pixel map iteratively
+
+        wfs - search window size in pixels
+        l_scale - length scale in pixels
+        n_iter - number of iteration
+        verbose - verbosity
+        """
+        obj = self.update_reference(l_scale=l_scale)
+        errors = []
+        for it in range(1, n_iter + 1):
+            errors.append(obj.mse())
+            if verbose:
+                print('Iteration No. {:d}: MSE = {:.5f}'.format(it, errors[-1]))
+            obj.update_pixel_map.inplace_update(wfs=wfs, l_scale=l_scale)
+            wfs = int(np.max(np.abs(obj.pix_map - np.indices(obj.whitefield.shape))))
+            if verbose:
+                print('Iteration No. {:d}: Search window size = {:d}'.format(it, wfs))
+            obj.update_reference.inplace_update(l_scale=l_scale)
+        return obj, errors
+   
