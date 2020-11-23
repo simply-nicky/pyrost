b686ff4c87b224a761f6aa8e7101ecda82a1408b
Nikolay Ivanov <simply_nicky@nikolays-mbp.localdomain>
2020-11-23 20:25:03 +0100

    readthedocs configuration file added

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ if USE_CYTHON:
                             libraries=cython_gsl.get_libraries(),
                             library_dirs=[cython_gsl.get_library_dir(), '/usr/local/lib'],
                             include_dirs=[numpy.get_include(),
-                                          cython_gsl.get_cython_include_dir()]),
+                                          cython_gsl.get_cython_include_dir()],
+                            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
                   Extension(name='st_utils',
                             sources=["robust_speckle_tracking/bin/st_utils.pyx"],
                             language="c",
@@ -28,13 +29,15 @@ if USE_CYTHON:
                             libraries=cython_gsl.get_libraries(),
                             library_dirs=[cython_gsl.get_library_dir(), '/usr/local/lib'],
                             include_dirs=[numpy.get_include(),
-                                          cython_gsl.get_cython_include_dir()])]
+                                          cython_gsl.get_cython_include_dir()],
+                            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])]
     extensions = cythonize(extensions, annotate=False, language_level="3",
                            build_dir="build",
                            compiler_directives={'cdivision': True,
                                                 'boundscheck': False,
                                                 'wraparound': False,
-                                                'binding': True})
+                                                'binding': True,
+                                                'embedsignature': True})
 else:
     extensions = [Extension(name="robust_speckle_tracking/bin/*",
                             sources=["robust_speckle_tracking/bin/*.c"],
