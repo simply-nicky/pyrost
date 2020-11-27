08ad3ee5b96be7314cd95652fdd025e85e22d7fb
simply-nicky <simply.i.nicky@gmail.com>
2020-11-27 16:11:11 +0100

    version changed

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Nikolay Ivanov'
 author = 'Nikolay Ivanov'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1b'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
