6bc198133a07dabd02a5d901aa4aa5bf5d2316a1
simply-nicky <simply.i.nicky@gmail.com>
2021-01-22 17:58:55 +0100

    tests updated, bug fixes, v 0.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Nikolay Ivanov'
 author = 'Nikolay Ivanov'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.1'
 
 
 # -- General configuration ---------------------------------------------------
